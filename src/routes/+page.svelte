b500de5ae5a516d26d95aed0c67b3e9abcfe3614
Elias Rissanen Uran <elura001@osloskolen.no>
2023-12-18 18:52:59 +0100

    cleanup of some code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,7 @@
 	import { apiUrl } from '$lib/host.js';
 	import { afterUpdate } from 'svelte';
 	import Icon from '@iconify/svelte';
-	import LatestPosts from '$lib/components/home/latestPosts.svelte';
-	import PopularPosts from '$lib/components/home/popularPosts.svelte';
+	import Post from '$lib/components/home/post.svelte';
 	import FilterField from '$lib/components/home/filterField.svelte';
 	import FilterResults from '$lib/components/home/filterResults.svelte';
 	import type { PageData } from './$types';
@@ -47,14 +46,14 @@
 		<h1 class="absolute -top-7 left-14 text-5xl dark:bg-bg bg-bgLight px-4 text-xdd">
 			latest posts
 		</h1>
-		{#each latestPostsData.slice(0, 2) as postData (postData.id)}<LatestPosts {postData} />{/each}
+		{#each latestPostsData.slice(0, 2) as postData (postData.id)}<Post {postData} />{/each}
 	</div>
 
 	<div class="relative border-t-4 border-xdd w-full h-96 pt-6 flex justify-around items-center">
 		<h1 class="absolute -top-7 right-14 text-5xl dark:bg-bg bg-bgLight px-4 text-xdd">
 			popular this week
 		</h1>
-		{#each popularPostsData as postData (postData.id)}<PopularPosts {postData} />{/each}
+		{#each popularPostsData as postData (postData.id)}<Post {postData} />{/each}
 	</div>
 
 	<div class="relative border-t-4 border-xdd w-full pt-16 flex flex-col gap-8 items-center">
--- src/routes/users/[user]/+page.svelte
+++ src/routes/users/[user]/+page.svelte
@@ -1,91 +1,18 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+
 	import TabContainer from '$lib/components/user/tabContainer.svelte';
-	import LatestPosts from '$lib/components/home/latestPosts.svelte';
-
-	import Icon from '@iconify/svelte';
-	const iconColor = '#E82A93';
-	const iconHeight = 25;
-
-	export let data;
-
-	let owner = false;
-	if (data.user_name === data.userInfo.name) {
-		owner = true;
-	}
-
-	interface Post {
-		likes: number;
-		date: string;
-	}
-	let userPosts: Post[] = data.posts.sort((a: Post, b: Post) => {
-		return b.likes - a.likes;
-	});
-
-	let selected = true;
+	import Profile from '$lib/components/user/profile/profile.svelte';
 </script>
 
 <TabContainer />
 
-<div class="mt-20 p-24 flex flex-col gap-8">
-	<div class="border-b-4 border-xdd pb-8 flex justify-between items-end">
-		<div class="flex gap-4 items-end">
-			<div class="border-4 border-xdd h-32 aspect-square rounded-md" />
-			<h1 class="text-6xl">{data.userInfo.name}</h1>
-		</div>
-		<!-- make post button just here temporarily -->
-		<div class="flex flex-col gap-2">
-			{#if owner}
-				<button class="rounded-md bg-xdd p-2 px-4 text-xl flex gap-2 items-center"
-					>Settings<Icon icon="solar:settings-outline" height={iconHeight} /></button
-				>
-			{/if}
-			<button class="rounded-md bg-xdd p-2 px-4 text-xl flex gap-2 items-center"
-				>New Post<Icon icon="lucide:book" height={iconHeight} /></button
-			>
-		</div>
-	</div>
-	<div class="flex justify-between w-full">
-		<div>
-			<h1 class="text-4xl">About</h1>
-			<p class="text-xl">{data.userInfo.bio}</p>
-		</div>
-		<div class="w-1/2 flex flex-col gap-8">
-			<div
-				class="h-16 text-lg dark:bg-primary bg-primaryLight border dark:border-secondary border-primaryLight rounded-md flex items-center justify-between overflow-hidden"
-			>
-				<button
-					on:click={() => {
-						selected = true;
-						userPosts = userPosts.sort((a, b) => {
-							return b.likes - a.likes;
-						});
-					}}
-					class={`${
-						selected && 'dark:bg-secondary bg-secondaryLight'
-					} w-1/2 p-5 flex justify-center gap-2`}
-					>popular<Icon icon="mdi:heart-outline" color={iconColor} height={iconHeight} /></button
-				>
-				<button
-					on:click={() => {
-						selected = false;
-						userPosts = userPosts.sort((a, b) => {
-							return new Date(b.date) - new Date(a.date);
-						});
-					}}
-					class={`${
-						!selected && 'dark:bg-secondary bg-secondaryLight'
-					} w-1/2 p-5 flex justify-center gap-2`}
-					>new<Icon
-						icon="mdi:lightning-bolt-outline"
-						color={iconColor}
-						height={iconHeight}
-					/></button
-				>
-			</div>
-			<div class="flex flex-col gap-14">
-				{#each userPosts.slice(0, 3) as postData}<LatestPosts {postData} />{/each}
-				<a class="text-blue-400 self-end" href={`/users/${data.userInfo.name}`}>See more..</a>
-			</div>
-		</div>
-	</div>
-</div>
+{#if $page.url.searchParams.get('tab') === 'Posts'}
+	<h2 class="mt-20 p-24 flex flex-col gap-4">
+		<div
+			class="h-16 text-lg dark:bg-primary bg-primaryLight border dark:border-secondary border-primaryLight rounded-md flex items-center"
+		/>
+	</h2>
+{:else}
+	<Profile />
+{/if}
